508a177449aa022cf02e9dcb814e8081200893ec
Raquel de Marichalar <rmarale445@g.educaand.es>
2022-02-17 09:38:37 +0100

    añado cambios en main

--- src/main/java/com/grupomixto/Supercalculadora.java
+++ src/main/java/com/grupomixto/Supercalculadora.java
@@ -82,7 +82,6 @@ public class Supercalculadora {
 
                         System.out.println("El resultado es: " + Pitagoras.Teorema(numA, numB));
                         break;
-
                     case 3:
                         System.out.println("Elige entre los siguientes polígonos para conocer su área: ");
                         System.out.println("Cuadrado (1), triángulo (2),círculo (3),hexágono(4),rectángulo (5)");
@@ -182,7 +181,7 @@ public class Supercalculadora {
 
                                 }
 
-                                /** Fin codificación:RMA 10/02/2022 **/
+                                /** Fin codificación:RMA 17/02/2022 **/
                                 /** Fin codificación:LAE 10/02/2022 **/
 
                         }
